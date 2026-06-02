c2d71eaf866c6ea5efb597fbce84efd43e17d12e
Victor.Li <victor.li@budweiserapac.com>
2026-06-02 16:47:16 -0400

    Number of islands for 200

---
+++ code/src/main/java/com/leepans/code/graph/A_NumberOfIslands_200.java
@@ -0,0 +1,66 @@
+package com.leepans.code.graph;
+
+/**
+ * @Author Victor
+ * @Date 2026-06-02 4:26 p.m.
+ */
+
+public class A_NumberOfIslands_200 {
+    public static int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int m = grid.length;
+        int n = grid[0].length;
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private static void dfs(char[][] grid, int i, int j) {
+        int m = grid.length;
+        int n = grid[0].length;
+        if (i < 0 || i >= m || j < 0 || j >= n) {
+            return;
+        }
+        if (grid[i][j] != '1') {
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+
+
+    public static void main(String[] args) {
+
+        char[][] grid = {
+                {'1', '1', '0', '0', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '1', '0', '0'},
+                {'0', '0', '0', '1', '1'}
+        };
+
+        int result = numIslands(grid);
+
+        System.out.println("Nums: " + result);
+
+        grid = new char[][]{
+                {'1', '1', '1', '1', '0'},
+                {'1', '1', '0', '1', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '0', '0', '0'}};
+        result = numIslands(grid);
+
+        System.out.println("Nums: " + result);
+    }
+}
