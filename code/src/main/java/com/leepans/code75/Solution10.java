84f4fbce9924239e5bf0c064965631a86e0fd3c7
Victor.Li <victor.li@budweiserapac.com>
2026-06-08 23:31:06 -0400

    283. Move Zeroes

---
+++ code/src/main/java/com/leepans/code75/Solution10.java
@@ -0,0 +1,21 @@
+package com.leepans.code75;
+
+/**
+ * @Author Victor
+ * @Date 2026-06-08 11:30 p.m.
+ */
+
+public class Solution10 {
+    public void moveZeroes(int[] nums) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        for (int i = index; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
