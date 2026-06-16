b3dc6023584975bef6e291e209a3b8169cb465c4
Victor.Li <victor.li@budweiserapac.com>
2026-06-16 17:40:41 -0400

    394. Decode String

--- code/src/main/java/com/leepans/code75/Solution26.java
+++ code/src/main/java/com/leepans/code75/Solution26.java
@@ -41,7 +41,7 @@ public class Solution26 {
     }
 
     private boolean isNum(char c) {
-        if (c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9') {
+        if (c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9') {
             return true;
         }
         return false;
