849f1c141fd26d933807fc729c5a3692947c7ace
Victor.Li <victor.li@budweiserapac.com>
2026-06-19 18:46:50 -0400

    236. Lowest Common Ancestor of a Binary Tree

---
+++ code/src/main/java/com/leepans/code75/Solution38.java
@@ -0,0 +1,31 @@
+package com.leepans.code75;
+
+/**
+ * @Author Victor
+ * @Date 2026-06-19 6:29 p.m.
+ */
+
+public class Solution38 {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        }
+        return left != null ? left : right;
+
+    }
+}
