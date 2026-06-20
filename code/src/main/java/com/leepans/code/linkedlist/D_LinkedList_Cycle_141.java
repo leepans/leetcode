eca1ea785f28c8224d1ae04aa701003f802fbead
Victor.Li <victor.li@budweiserapac.com>
2026-06-20 16:26:01 -0400

    LinkedList has a cycle 141

---
+++ code/src/main/java/com/leepans/code/linkedlist/D_LinkedList_Cycle_141.java
@@ -0,0 +1,33 @@
+package com.leepans.code.linkedlist;
+
+/**
+ * @Author Victor
+ * @Date 2026-06-20 4:22 p.m.
+ */
+
+public class D_LinkedList_Cycle_141 {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
