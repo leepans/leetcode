b2ddbe76899a757087ba971a30c88eb446cbf1ad
Victor.Li <victor.li@budweiserapac.com>
2026-06-26 17:25:55 -0400

    199. Binary Tree Right Side View

---
+++ code/src/main/java/com/leepans/code75/Solution39.java
@@ -0,0 +1,51 @@
+package com.leepans.code75;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @Author Victor
+ * @Date 2026-06-26 5:20 p.m.
+ */
+
+public class Solution39 {
+    class TreeNode {
+        TreeNode left;
+        TreeNode right;
+        int val;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                if (i == size - 1) {
+                    result.add(node.val);
+                }
+            }
+        }
+        return result;
+    }
+}
